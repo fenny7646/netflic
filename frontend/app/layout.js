270c911e5140cfedc3fa83c94578a95ee7915759
fenny7646 <fennyvarghese1@gmail.com>
2025-04-15 09:35:25 +0530

    Headers up

---
+++ frontend/app/layout.js
@@ -0,0 +1,11 @@
+import './globals.css';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="App">
+        {children}
+      </body>
+    </html>
+  );
+}
